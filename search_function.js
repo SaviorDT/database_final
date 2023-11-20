30c3a301e01e287504ebfb1c72f7a8887764f8f3
ChenYen-Yen <144677402+ChenYen-Yen@users.noreply.github.com>
2023-11-20 08:39:04 -0600

    Add files via upload

--- search_function.js
+++ search_function.js
@@ -20,12 +20,23 @@ function date_form(){
         document.getElementById("second_date").innerHTML = ' ';
     }
 }
-
 function make_table(album_obj){
     let tbl = document.getElementById("albums");
+    //name first, then value
+    let total_column = album_obj.description.length;
     let new_row = tbl.insertRow(-1);
-    let view_but = new_row.insertCell(-1);
-    let delete_but = new_row.insertCell(-1);
-    view_but.innerHTML = '<input type="button" name="'+album_obj.albums_id+'" onclick="view_full()" value="view"></input>';
-    delete_but.innerHTML = '<input type="button" name="'+album_obj.albums_id+'" onclick="delete_this(this)" value="delete"></input>';
+    for(let j=0;j<total_column;j++){
+        let new_cell = new_row.insertCell(-1);
+        new_cell.innerHTML = album_obj.description[j];
+    }
+    for(let i=0;i<total_column;i++){
+        for(let j=0;j<total_column;j++){
+            let new_cell = new_row.insertCell(-1);
+            new_cell.innerHTML = album_obj.rows[j];
+        }
+        let view_but = new_row.insertCell(-1);
+        let delete_but = new_row.insertCell(-1);
+        view_but.innerHTML = '<input type="button" name="'+album_obj.albums_id+'" onclick="view_full()" value="view"></input>';
+        delete_but.innerHTML = '<input type="button" name="'+album_obj.albums_id+'" onclick="delete_this(this)" value="delete"></input>';
+    }
 }
